fd8fe0d8a478eb2a37c865ff5bbcfb80b143dc38
Arturmi <artur.trombo@gmail.com>
2024-10-17 12:41:36 +0200

    arreglat DWS cvlase

--- PHP3/E3_formreentrant/E3_multiplicarSelf.php
+++ PHP3/E3_formreentrant/E3_multiplicarSelf.php
@@ -6,22 +6,24 @@
 </head>
 <body>
 <?php
-if(true){
-    echo "<table align='left' border='1px' padding='1px'>";
+if(isset($_GET['numero']))
+{   echo "<table align='left' border='1px' padding='1px'>";
 
-    $numero = $_GET["numero"];
+    $numero = $_GET['numero'];
     echo "<h4>VER LA TABLA DEL $numero.</h4>";
     for ($i = 0; $i < 11; $i++){
         echo "<tr><td>$numero X $i</td><td>" . $numero*$i . "</td>";
     }
 
     echo "</table>";
-}else {
+}    
+else {
     for($i = 0; $i < 11; $i++){
-        echo "<a href='E3_multiplicarSelf.php?numero=$i'>Ver la tabla del $i</a>" . "<br>";
+        echo "<a href='E3_multiplicarSelf.php?numero=$i'>Ver la tabla del $i</a>".'<br>';
     }
 }
 ?>
+
 </body>
 </html>
 
